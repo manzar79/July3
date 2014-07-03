3b7db89504e1993fb271ab170e2a5d8506e2babc
manzar <manzar79@gmail.com>
2014-07-03 10:56:59 -0400

    this
    this

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car;
+
+            car = new Car("red");
+            Console.WriteLine(car.Describe());
+
+            car = new Car("blue");
+            Console.WriteLine(car.Describe());
+
+            Console.ReadLine();
+
+
+        }
+
+        class Car
+        {
+            private string color; 
+        
+            public Car (String color)
+            {
+                this.color = color;
+            }
+            
+        
+
+            public string Describe()
+            {
+            
+                return "this color is" + Color;
+            }
+
+            public string Color {
+                get { return color; }
+
+                set { color = value; }
+            }
+        }
+    }
+}
---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication2
+{
+    class Employee
+    {
+        private string name;
+        private string alias;
+        private decimal salary = 3000.00m;
+
+        // Constructor: 
+        public Employee(string name, string alias)
+        {
+            // Use this to qualify the fields, name and alias: 
+            this.name = name;
+            this.alias = alias;
+        }
+        // Printing method: 
+        public void printEmployee()
+        {
+            Console.WriteLine("Name: {0}\nAlias: {1}", name, alias);
+            // Passing the object to the CalcTax method by using this:
+            Console.WriteLine("Taxes: {0:C}", Tax.CalcTax(this));
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+        }
+    }
+
+    class Tax
+    {
+        public static decimal CalcTax(Employee E, Employee F)
+        {
+            return 0.08m * E.Salary;
+            
+        }
+    }
+
+    class MainClass
+    {
+        static void Main()
+        {
+            // Create objects:
+            Employee E1 = new Employee("Mingda Pan", "mpan");
+            Employee E2 = new Employee("Manzer tan", "mtan");
+
+            // Display results:
+            E1.printEmployee();
+            E2.printEmployee();
+
+            Console.ReadLine();
+        }
+    }
+}
